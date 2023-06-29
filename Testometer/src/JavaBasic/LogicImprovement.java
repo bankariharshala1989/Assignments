cd6141144f6bca8dc43ae7b6ff2b6aa103371422
Devansh <Devansh@LAPTOP-JU354M2T>
2023-06-29 22:57:24 +0530

    Write a code to make 17 with available operators only You have given below number and operators number - 1,2,6,7 operators - +,-,*

---
+++ Testometer/src/JavaBasic/LogicImprovement.java
@@ -0,0 +1,30 @@
+package JavaBasic;
+
+public class LogicImprovement {
+
+	public static void main(String[] args) {
+		/*
+		 
+Write a code to make 17 with available operators only
+You have given below number and operators
+number - 1,2,6,7
+operators - +,-,*
+
+You can't repeat any number
+and you can't skip any operator and number
+You have to create 17 using above rule
+		 */
+		
+		int a=1;
+		int b=2;
+		int c=6;
+		int d=7;
+		
+		int ans;
+		ans= ((c-a)*b)+d;
+
+
+		System.out.println(ans);
+	}
+
+}
