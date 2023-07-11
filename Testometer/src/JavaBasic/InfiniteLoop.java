5ee82827f005a72fc1e7a956294b49466677ae29
Devansh <Devansh@LAPTOP-JU354M2T>
2023-07-11 10:49:59 +0530

    Write 2 ways to write infinite for loop

---
+++ Testometer/src/JavaBasic/InfiniteLoop.java
@@ -0,0 +1,16 @@
+package JavaBasic;
+
+public class InfiniteLoop {
+
+	public static void main(String[] args) {
+		//for(;;) {
+		//	System.out.println("hello");
+		//}
+
+		for(int i=0; i<3;i--) {
+			System.out.println("hello");
+		}
+		
+		
+	}
+}
