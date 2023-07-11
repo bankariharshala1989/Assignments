0eef1487a88f64271e05507d6f6d6aa0dc406842
Devansh <Devansh@LAPTOP-JU354M2T>
2023-07-11 12:37:20 +0530

    Define String

---
+++ Testometer/src/String/Test1.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		/* Define String
+		String is array of character
+		String s1 ="Harshala";
+		*/
+
+	}
+
+}
