713ac7b90f72f51d8cbf2d6d9f8b313193dacf49
Devansh <Devansh@LAPTOP-JU354M2T>
2023-07-13 21:00:46 +0530

    Write code to understand this constructor should be first call ?

---
+++ Testometer/src/Constructor/Test3.java
@@ -0,0 +1,20 @@
+package Constructor;
+
+public class Test3 {
+public static void main(String[] args) {
+	D d=new D();	
+}
+	
+}
+class D{
+
+	D(){
+		this(3);
+		System.out.println("Hello");
+		
+		}
+
+	D(int i){
+
+		}
+}
