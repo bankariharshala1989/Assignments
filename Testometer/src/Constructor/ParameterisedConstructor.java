1cd8d1557b78bbf95e2e4d48096341fe05393567
Devansh <Devansh@LAPTOP-JU354M2T>
2023-07-13 21:23:35 +0530

    Write a code to call parameterised constructor from default constructor?

---
+++ Testometer/src/Constructor/ParameterisedConstructor.java
@@ -0,0 +1,18 @@
+package Constructor;
+
+public class ParameterisedConstructor {
+
+}
+
+class A{
+
+	A(){
+		// Write your code here to call A(int i) constructor
+		this(5);	
+	}
+
+	A(int i){
+		
+		}
+
+}
